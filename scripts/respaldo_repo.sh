1c898ddc9e01ceb75843347d247489b774762f88
Abel Barrios <barriosabel0@protonmail.com>
2025-08-29 19:00:01 -0300

    Backup del archivo 2025-08-29 19:00:01

--- scripts/respaldo_repo.sh
+++ scripts/respaldo_repo.sh
@@ -3,7 +3,7 @@
 
 # Variables
 
-git pull
+#git pull
 
 archivo_a_copiar="/home/mycsrl/proyectos/mycsrl/db.sqlite3"  # Cambia esto por la ruta de tu archivo
 directorio_respaldo="/home/mycsrl/proyectos/mycsrl/bck/bk_db.sqlite"    # Cambia esto por la ruta del directorio de respaldo
